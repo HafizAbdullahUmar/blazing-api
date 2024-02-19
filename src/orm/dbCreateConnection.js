77903a9c705ce1ff260d501e9bdd73fcf7dca69a
Abdullah Umar <108179811+HafizAbdullahUmar@users.noreply.github.com>
2024-02-19 07:30:03 +0500

    update

--- src/orm/dbCreateConnection.js
+++ src/orm/dbCreateConnection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
 
-export const dbCreateConnection = async () => {
+ const dbCreateConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Database connection success.');
@@ -12,3 +12,4 @@ export const dbCreateConnection = async () => {
   }
   return null;
 };
+module.exports = dbCreateConnection
